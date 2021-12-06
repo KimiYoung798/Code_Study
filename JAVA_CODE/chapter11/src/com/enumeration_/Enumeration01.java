87ec5efc9a74150144911a729c7078f38a541313
Kimi <1390405753@qq.com>
2021-12-06 15:35:51 +0800

    枚举结束

---
+++ JAVA_CODE/chapter11/src/com/enumeration_/Enumeration01.java
@@ -0,0 +1,82 @@
+package com.enumeration_;
+
+public class Enumeration01 {
+    public static void main(String[] args) {
+        System.out.println(Season.AUTUMN);
+        System.out.println(Season.SPRING);
+    }
+
+}
+
+//自定义枚举实现
+class Season {
+    private String name;
+    private String desc;
+
+    //定义了四个对象, 固定.
+    public static final Season SPRING = new Season("春天", "温暖");
+    public static final Season WINTER = new Season("冬天", "寒冷");
+    public static final Season AUTUMN = new Season("秋天", "凉爽");
+    public static final Season SUMMER = new Season("夏天", "炎热");
+
+    private Season(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
+
+//使用enum关键字
+enum Season2 {
+//    private String name;
+//    private String desc;
+
+    //定义了四个对象, 固定.
+//    public static final Season SPRING = new Season("春天", "温暖");
+//    public static final Season WINTER = new Season("冬天", "寒冷");
+//    public static final Season AUTUMN = new Season("秋天", "凉爽");
+//    public static final Season SUMMER = new Season("夏天", "炎热");
+
+    SPRING("春天", "温暖"),
+    WINTER("冬天", "寒冷"),
+    AUTUMN("秋天", "凉爽"),
+    SUMMER("夏天", "炎热");
+    private String name;
+    private String desc;
+
+    private Season2(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
